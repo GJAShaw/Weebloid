c2cab76fe31ff8e049f033943b3238c7c46ddf94
GJAShaw <32239237+GJAShaw@users.noreply.github.com>
2018-01-15 14:12:40 +0000

    Create HelloWeeble.java
    Initial commit; HelloWeeble v0.01 class.

---
+++ HelloWeeble.java
@@ -0,0 +1,16 @@
+package com.weebling.weeblish
+/**
+ * @author Greg Shaw
+ *
+/*
+class HelloWeeble {
+  
+  /**
+   * @params (none)
+   *
+  public static void(String[] args){
+  
+    System.out.println("HelloWeeble class");
+    
+  }
+}
